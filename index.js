47cc48b52e0e9bb0af95c480cbf91baeac4305c9
Andrew Hanna <percyhanna@gmail.com>
2014-04-07 11:09:28 -0600

    Must register as Class method, not Instance method.

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ var Mincer      = require('mincer');
 var Template    = Mincer.Template;
 var prop        = require('mincer/lib/mincer/common').prop;
 var path        = require('path');
-var sh = require('execSync');
+var sh          = require('execSync');
+var includeDirs = [];
 
 ////////////////////////////////////////////////////////////////////////////////
 
 // Class constructor
 var RubySassEngine = module.exports = function RubySassEngine() {
   Template.apply(this, arguments);
-  this._includeDirs = [];
 };
 
 function shellEscape (text) {
@@ -36,16 +36,16 @@ function shellEscape (text) {
 
 require('util').inherits(RubySassEngine, Template);
 
-RubySassEngine.prototype.addIncludeDir = function (dir) {
-  this._includeDirs.push(dir);
+RubySassEngine.addIncludeDir = function (dir) {
+  includeDirs.push(dir);
 };
 
 // Render data
 RubySassEngine.prototype.evaluate = function (context, locals) {
   var data = shellEscape(this.data),
       dir = path.dirname(this.file),
-      includeDirs = this._includeDirs.concat(dir).map(shellEscape),
-      css = sh.exec('echo ' + data + ' | sass -s -q --scss -I ' + includeDirs.join(' -I '));
+      dirs = includeDirs.concat(dir).map(shellEscape),
+      css = sh.exec('echo ' + data + ' | sass -s -q --scss -I ' + dirs.join(' -I '));
 
   if (css.code) {
     throw new Error(css.stdout);
